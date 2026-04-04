41712b8bd26f82ea16ef407a0b2735c39b9d062b
Rupak Kulkarni <rk268@njit.edu>
2026-04-03 23:16:20 -0400

    Time: 363 ms (50.17%) | Memory: 0B (100.00%) - LeetSync

--- 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
+++ 601-human-traffic-of-stadium/human-traffic-of-stadium.sql
@@ -1,39 +1,15 @@
-WITH d AS (
-    SELECT id, visit_date, people
+WITH t AS (
+    SELECT *,
+           LEAD(id,1) OVER (ORDER BY id) AS id1,
+           LEAD(id,2) OVER (ORDER BY id) AS id2,
+           LAG(id,1)  OVER (ORDER BY id) AS pid1,
+           LAG(id,2)  OVER (ORDER BY id) AS pid2
     FROM Stadium
     WHERE people >= 100
-),
-t AS (
-    SELECT id,
-           visit_date,
-           people,
-           LEAD(id, 1) OVER (ORDER BY id) AS id1,
-           LEAD(id, 2) OVER (ORDER BY id) AS id2
-    FROM d
-),
-valid_ids AS (
-    SELECT id
-    FROM t
-    WHERE id1 = id + 1
-      AND id2 = id + 2
-
-    UNION
-
-    SELECT id1
-    FROM t
-    WHERE id1 = id + 1
-      AND id2 = id + 2
-
-    UNION
-
-    SELECT id2
-    FROM t
-    WHERE id1 = id + 1
-      AND id2 = id + 2
 )
-
-SELECT s.id, s.visit_date, s.people
-FROM d s
-JOIN valid_ids v
-  ON s.id = v.id
-ORDER BY s.visit_date;
+SELECT id, visit_date, people
+FROM t
+WHERE (id1 = id + 1 AND id2 = id + 2)
+   OR (pid1 = id - 1 AND id1 = id + 1)
+   OR (pid2 = id - 2 AND pid1 = id - 1)
+ORDER BY visit_date;
