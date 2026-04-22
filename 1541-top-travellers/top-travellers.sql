b2b497613e812c97169199a6abac64f863fb67ff
Rupak Kulkarni <rk268@njit.edu>
2026-04-21 22:48:18 -0400

    Time: 820 ms (50.78%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+Select u.name, coalesce(sum(r.distance),0) as travelled_distance
+from Users u
+left join Rides r on u.id = r.user_id
+group by u.id
+order by travelled_distance desc, u.name asc 
